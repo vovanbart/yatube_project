0336de372edad38bbc718aaa32e4e301a573b226
vovanbart <vovanbart@icloud.com>
2021-08-10 20:10:10 +0300

    Первая попытка

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,11 @@
+# yatube/posts/urls.py
 from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path("", views.index),
-    path("group/any_slug/", views.group_posts),
+    path("", views.index, name='index'),
+    path("group/<slug:slug>/", views.group_posts, name='group'),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,24 @@
-from django.http.response import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
-def group_posts(request):
-    return HttpResponse('Посты по группам')
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    text = 'Страница о группах проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
